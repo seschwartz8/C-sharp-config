f4aa6aa78e8d8a0e3e65ff8fd90ba969c5160ca3
Sarah Schwartz <seschwartz8@gmail.com>
2020-03-07 21:26:49 -0800

    create form and display user inputs on result route

--- ProjectName/Controllers/HomeController.cs
+++ ProjectName/Controllers/HomeController.cs
@@ -6,20 +6,19 @@ namespace ProjectName.Controllers
   public class HomeController : Controller
   {
     [Route("/")]
-    public ActionResult Landing()
+    // The line above is a route decorator -- it overrides needing to go to home/goodbye, instead just /goodbye
+    public string Landing() { return "This is the landing page!"; }
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/result")]
+    public ActionResult Result(string name, string city)
     {
-      ClassName instance = new ClassName("Sasa");
-      instance.City = "Portland";
-      string newName = "Tessa";
-      instance.Name = newName;
+      ClassName instance = new ClassName("DefaultName");
+      instance.Name = name;
+      instance.City = city;
       return View(instance);
     }
-
-    [Route("/hello")]
-    public string Hello() { return "Hello!"; }
-
-    [Route("/goodbye")]
-    // The line above is a route decorator -- it overrides needing to go to home/goodbye, instead just /goodbye
-    public string Goodbye() { return "Goodbye!"; }
   }
 }
