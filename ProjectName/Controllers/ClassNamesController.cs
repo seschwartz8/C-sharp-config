146673efda4d332337b2e5ba34a7a3deef474a20
Sarah Schwartz <seschwartz8@gmail.com>
2020-03-11 15:34:33 -0700

    update config to include new REST methods

--- ProjectName/Controllers/ClassNamesController.cs
+++ ProjectName/Controllers/ClassNamesController.cs
@@ -5,16 +5,43 @@ namespace ProjectName.Controllers
 {
   public class ClassNamesController : Controller
   {
-    [HttpGet("/form")]
-    public ActionResult Form() { return View(); }
-
-    [HttpPost("/result")]
-    public ActionResult Result(string name, string city)
+    [HttpGet("/objects")]
+    public ActionResult Index()
     {
-      ClassName instance = new ClassName("DefaultName");
-      instance.Name = name;
-      instance.City = city;
-      return View(instance);
+      //Displays list of places
+      List<ClassName> allObjects = ClassName.GetAll();
+      return View(allObjects);
+    }
+
+    [HttpGet("/objects/new")]
+    public ActionResult New()
+    {
+      //Offers form to create new place
+      return View();
+    }
+
+    [HttpPost("/objects")]
+    public ActionResult Create(string description)
+    {
+      //Creates new ClassName
+      Place myClassName = new ClassName(description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/objects/delete")]
+    public ActionResult DeleteAll()
+    {
+      //Clears all ClassNames
+      ClassName.ClearAll();
+      return View();
+    }
+
+    [HttpGet("/objects/{id}")]
+    public ActionResult Show(int id)
+    {
+      //Displays one ClassName's specific details
+      ClassName foundClassName = ClassName.Find(id);
+      return View(foundClassName);
     }
   }
 }
--- ProjectName/Models/ClassName.cs
+++ ProjectName/Models/ClassName.cs
@@ -5,12 +5,10 @@ namespace ProjectName.Models
   public class ClassName
   {
     public string Name { get; set; }
-    public string City { get; set; }
 
     public ClassName(string name)
     {
       Name = name;
-      City = "Seattle";
     }
 
     public bool MethodName()
